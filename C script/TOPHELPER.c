70ac3e1745cf8932be8b4e4955794976d28d8fd6
LYNBYYDS <94834425+LYNBYYDS@users.noreply.github.com>
2023-09-19 20:10:17 +0200

    Update the C script

--- C script/TOPHELPER.c
+++ C script/TOPHELPER.c
@@ -75,7 +75,7 @@ void connection_Kernel_rbf(FILE * fp)
         }
         for (j = 0; j <= 7; j++)
         {
-            fprintf(fp,"test_data_dim%d => test_data_dim0%d,\n\t\t\t\t", j, j);
+            fprintf(fp,"test_data_dim%d => test_data_dim%d,\n\t\t\t\t", j, j);
         }
         fprintf(fp,"gamma => gamma,\n\t\t\t\t");
         fprintf(fp,"output => Kernel_rbf_output_s%d\n\t\t\t\t",i);
@@ -91,6 +91,13 @@ void multiplier_signal(FILE * fp)
     {
         fprintf(fp,"signal multi_s%d : std_logic_vector(12 downto 0);\n", i);
     }
+    
+    fprintf(fp,"\n");
+
+    for (i = 0; i <= 70; i++)
+    {
+        fprintf(fp,"signal multi_20bits_s%d : std_logic_vector(19 downto 0);\n", i);
+    }
 }
 
 void connection_multiplier(FILE * fp)
@@ -101,7 +108,8 @@ void connection_multiplier(FILE * fp)
         fprintf(fp,"\n\tMultiplier_Signed_12bits_%d : Multiplier_Signed_12bits\n\tport map(\t", i);
         
         fprintf(fp,"input_a => SV%d_alphaY,\n\t\t\t\t", i);
-        fprintf(fp,"input_b => '0' & Kernel_rbf_output_s%d,\n\t\t\t\t", i);
+        fprintf(fp,"input_b(11) => '0',\n\t\t\t\t");
+        fprintf(fp,"input_b(10 downto 0) => Kernel_rbf_output_s%d,\n\t\t\t\t", i);
         fprintf(fp,"output => multi_s%d\n\t\t\t\t", i);
         fprintf(fp,");\n");
     }
@@ -155,6 +163,19 @@ void adder_signal(FILE * fp)
     fprintf(fp,"signal add_overflow_s60 : std_logic; \n");
 }
 
+void connection_convertor(FILE * fp)
+{
+    int i, j;
+    for (i = 0; i <= 70; i++)
+    {
+        fprintf(fp,"\n\tConvertor_Signed_13to20bits_%d : Convertor_Signed_13to20bits\n\tport map(\t", i);
+        
+        fprintf(fp,"input => multi_s%d,\n\t\t\t\t", i);
+        fprintf(fp,"output => multi_20bits_s%d\n\t\t\t\t", i);
+        fprintf(fp,");\n");
+    }
+}
+
 void connection_adder_0(FILE * fp)
 {
     int i, j;
@@ -162,15 +183,15 @@ void connection_adder_0(FILE * fp)
     {
         fprintf(fp,"\n\tAdder_Signed_20bits_0%d : Adder_Signed_20bits\n\tport map(\t", i);
         
-        fprintf(fp,"input_a => multi_s%d,\n\t\t\t\t", 2*i);
-        fprintf(fp,"input_b => multi_s%d,\n\t\t\t\t", 2*i+1);
+        fprintf(fp,"input_a => multi_20bits_s%d,\n\t\t\t\t", 2*i);
+        fprintf(fp,"input_b => multi_20bits_s%d,\n\t\t\t\t", 2*i+1);
         fprintf(fp,"output => add_output_s0%d,\n\t\t\t\t", i);
         fprintf(fp,"overflow => add_overflow_s0%d\n\t\t\t\t", i);
         fprintf(fp,");\n");
     }
     fprintf(fp,"\n\tAdder_Signed_20bits_035 : Adder_Signed_20bits\n\tport map(\t");
         
-        fprintf(fp,"input_a => multi_s70,\n\t\t\t\t");
+        fprintf(fp,"input_a => multi_20bits_s70,\n\t\t\t\t");
         fprintf(fp,"input_b => b,\n\t\t\t\t");
         fprintf(fp,"output => add_output_s035,\n\t\t\t\t");
         fprintf(fp,"overflow => add_overflow_s035\n\t\t\t\t");
@@ -252,7 +273,7 @@ void connection_adder_3plus(FILE * fp)
 int main(void)
 {    
     FILE *fp;                           // File pointer
-    fp = fopen("testtop.txt", "w");       // Open the file
+    fp = fopen("texttop.txt", "w");       // Open the file
     if(fp == NULL)                      // Check open error
     {
         printf("Error!");
@@ -264,8 +285,9 @@ int main(void)
     //adder_signal(fp);
     //multiplier_signal(fp);
     
-    //connection_Kernel_rbf(fp);
+    connection_Kernel_rbf(fp);
     //connection_multiplier(fp);
+    //connection_convertor(fp);
     //connection_adder_0(fp);
     //connection_adder_1(fp);
     //connection_adder_2(fp);
